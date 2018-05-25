884729f77a99d18421b9d0777d9dcd4e6ccf7df1
RDR96 <00231514@uca.edu.sv>
2018-05-25 17:55:23 -0600

    Modificacion al xml de la pantalla para agregar notas, se añadio el color principal (temporal) e icono para añadir nota

--- app/src/main/java/com/rdr/rodrigocorvera/personas/Activities/AddNoteActivity.java
+++ app/src/main/java/com/rdr/rodrigocorvera/personas/Activities/AddNoteActivity.java
@@ -2,14 +2,19 @@ package com.rdr.rodrigocorvera.personas.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Spinner;
 
 import com.rdr.rodrigocorvera.personas.R;
 
 public class AddNoteActivity extends AppCompatActivity {
 
+    Spinner categoriesSpinner;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_note);
+        
     }
 }
